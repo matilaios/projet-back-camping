04f7c669e53c1014159ee7fa0c029d40c537345d
JuNicolle <juliennicolle@hotmail.fr>
2024-12-18 16:32:39 +0100

    HELP ME

--- routes/crudTypeHebergement.js
+++ routes/crudTypeHebergement.js
@@ -7,11 +7,28 @@ const auth = require("../middleware/auth");
 
 // route lecture des hebergements
 router.get("/readTypeHebergement", (req, res) => {
-    const readTypeHebergement = "SELECT TYPE_HEBERGEMENT.nom, TYPE_HEBERGEMENT.description, TYPE_HEBERGEMENT.capacite FROM TYPE_HEBERGEMENT;";
-    bdd.query(readTypeHebergement, (error, results) => {
+    // const readTypeHebergement = "SELECT TYPE_HEBERGEMENT.nom, TYPE_HEBERGEMENT.description, TYPE_HEBERGEMENT.capacite FROM TYPE_HEBERGEMENT;";
+    const testHebergPhoto = "SELECT th.idTYPE, th.nom AS nom_hebergement, th.description AS description_hebergement, th.capacite AS capacite_hebergement, p.nom AS nom_photo, p.image FROM type_hebergement th LEFT JOIN photo p ON th.idTYPE = p.idTYPE;"
+    bdd.query(testHebergPhoto, (error, results) => {
       if (error) throw error;
       res.json(results);
     });
   });
 
-  module.exports = router;
+  //route lecture d'un hebergement par son ID
+  router.get("/readTypeHebergementById/:idTYPE", (req, res) => {
+    const { idTYPE } = req.params;
+    const readHebergementById = "SELECT th.idTYPE, th.nom AS nom_hebergement, th.description AS description_hebergement, th.capacite AS capacite_hebergement, p.nom AS nom_photo, p.image FROM type_hebergement th LEFT JOIN photo p ON th.idTYPE = p.idTYPE;";
+    bdd.query(readHebergementById, [idTYPE], (error, results) => {
+        if (error) {
+            console.error("Erreur lors de la requête SQL:", error);
+            return res.status(500).json({ message: "Erreur serveur." });
+        }
+        if (results.length === 0) {
+            return res.status(404).json({ message: "Aucun type d'hébergement trouvé pour cet ID." });
+        }
+        res.json(results[0]); // On retourne un seul objet
+    });
+});
+  
+    module.exports = router;
