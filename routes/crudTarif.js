dbdceff4cb4f677c38bf08b9ae0a9006360dc7d6
karine mullier <karine_mullier@hotmail.com>
2024-12-19 16:16:09 +0100

    crud tarif

--- routes/crudTarif.js
+++ routes/crudTarif.js
@@ -31,71 +31,63 @@ router.get("/readTarif", (req, res) => {
   });
 
 
-//route lecture d'un Tarif par son ID - FONTIONNE
+//route lecture d'un Tarif par son ID - FONCTIONNE
 router.get("/readTarifById/:idTarif", (req, res) => {
     const { idTarif } = req.params;
-    const readTarifById = "SELECT * FROM Tarif WHERE idTarif = ?;";
+    const readTarifById = "select tarif.idTarif, tarif.nom, tarif.prix, GROUP_CONCAT(type_hebergement.nom) as HEBERGEMENTS FROM tarif LEFT JOIN type_hebergement ON type_hebergement.idTarif=tarif.idTarif WHERE tarif.idTarif=? GROUP BY tarif.idTarif, tarif.nom, tarif.prix;";
     bdd.query(readTarifById, [idTarif], (error, results) => {
       if (error) throw error;
       res.json(results);
     });
   });
 
-// route MISE A JOURf par son ID
-router.post("/updateTarif/:idTarif",auth.authentification ,async (req, res) => {
+// route MISE A JOURf par son ID POUR LES ADMIN - FONCTIONNE
+router.post("/updateTarifByID/:idTarif",auth.authentification , (req, res) => {
+  const { idTarif } = req.params;
+  const { nom, prix } = req.body;
+  console.log(idTarif);
   if (req.role == false) {
     console.log("vous n'avez pas accès à cette fonctionnalité");
     res.status(403).json({ message: "Vous n'avez pas accès à cette fonctionnalité." });
   } else {
-    const { idTarif } = req.params;
-    const { nom, prix } = req.body;
+    console.log("j'ai les droits");
     const updateTarifById = "UPDATE tarif SET nom=?,prix=? WHERE idTarif=?;"
-
-  
-
-
-
-
-
-  // Récupérer les valeurs actuelles du tarif
-  const getTarif = "SELECT * FROM Tarif WHERE idTarif = ?;";
-  bdd.query(getTarif, [idTarif], (error, results) => {
-      if (error) throw error;
-      if (results.length === 0) {
-          return res.status(404).send("Tarif non trouvé");
-      }
-
-      const currentTarif = results[0];
-      const updatedNom = nom || currentTarif.nom;
-      const updatedPrix = prix || currentTarif.prix;
-
-      // Mettre à jour le tarif avec les nouvelles valeurs ou les anciennes si non fournies
-      const updateTarif = "UPDATE Tarif SET nom = ?, prix = ? WHERE idTarif = ?;";
-      bdd.query(updateTarif, [updatedNom, updatedPrix, idTarif], (error) => {
+    console.log("j'ai passé la const update");
+    bdd.query(updateTarifById, [nom, prix, idTarif], (error, results) => {
           if (error) throw error;
           res.send("Tarif mis à jour");
       });
-  })};
+  };
 });
 
-  // route suppression d'un tarif
-router.post("/deleteTarif/:idTarif", (req, res) => {
+
+
+
+
+  // route SUPPRESSION d'un tarif POUR LES ADMIN - FONCITONNE
+router.delete("/deleteTarifById/:idTarif", auth.authentification, (req, res) => {
+  const { idTarif } = req.params;
   if (req.role == false) {
     console.log("vous n'avez pas accès à cette fonctionnalité");
-    res.status(403).json({ message: "Vous n'avez pas accès à cette fonctionnalité." });
+    return res.status(401).send("Vous n'avez pas les droits pour supprimer un Tarif");
   } else {
-    if (req.role !== "admin") {
-      return res.status(401).send("Vous n'avez pas les droits pour supprimer un Tarif");
-    }
-    const { idTarif } = req.params;
-
-  
-    const deleteTarif = "DELETE FROM Tarif WHERE idTarif = ?;";
-    bdd.query(deleteTarif, [idTarif], (error, results) => {
+    const deleteTarifById = "DELETE FROM Tarif WHERE idTarif=?;";
+    bdd.query(deleteTarifById, [idTarif], (error, results) => {
       if (error) throw error;
       res.json(results);
     })};
-  });
+
+    });
+  
+  // ROUTE POURLIRE LES TARIFS DES HEBERGEMENTS TYPES POUR LA PAGE TARIF
+  router.get('/readAllTarifAllHebergement/', (req,res) => {
+  const readAllTarifAllHebergement="SELECT Type_hebergement.idTarif, Type_hebergement.nom AS nomType, type_hebergement.capacite, tarif.nom, tarif.prix FROM type_hebergement INNER JOIN TARIF on type_hebergement.idTarif=tarif.idTarif;";
+  bdd.query(readAllTarifAllHebergement, (error,result)=>{
+    if (error) throw error;
+    res.json(result);
+  })
+})
+
 
   module.exports = router;
 
